387b56893555373e3abac46e827383ff98361f65
cravo123 <cc3630@columbia.edu>
2019-02-17 18:17:21 -0500

    Create 0589. N-ary Tree Preorder Traversal.py

---
+++ 0589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,38 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+# Recursion
+class Solution:
+    def dfs(self, node, res):
+        if node is None:
+            return
+        res.append(node.val)
+        for p in node.children:
+            self.dfs(p, res)
+        
+    def preorder(self, root: 'Node') -> 'List[int]':
+        res = []
+        self.dfs(root, res)
+        return res
+
+# Iteration
+class Solution:
+    def preorder(self, root: 'Node') -> 'List[int]':
+        res = []
+        if root is None:
+            return res
+        
+        q = [root]
+        
+        while q:
+            p = q.pop()
+            res.append(p.val)
+            # Notice we need to reverse children so that it is pre-order
+            for x in reversed(p.children):
+                q.append(x)
+        return res

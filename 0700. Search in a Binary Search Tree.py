56289bf45c555d21dc27a24fa618e0061db3108e
cravo123 <cc3630@columbia.edu>
2019-02-17 18:20:05 -0500

    Create 0700. Search in a Binary Search Tree.py

---
+++ 0700. Search in a Binary Search Tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Recursion
+class Solution:
+    def dfs(self, node, val):
+        if node is None or node.val == val:
+            return node
+        if node.val < val:
+            return self.dfs(node.right, val)
+        return self.dfs(node.left, val)
+        
+    def searchBST(self, root: 'TreeNode', val: 'int') -> 'TreeNode':
+        return self.dfs(root, val)
+
+# Iteration
+class Solution:
+    def searchBST(self, root: 'TreeNode', val: 'int') -> 'TreeNode':
+        curr = root
+        
+        while curr:
+            if curr.val < val:
+                curr = curr.right
+            elif curr.val > val:
+                curr = curr.left
+            else:
+                return curr
+        return 

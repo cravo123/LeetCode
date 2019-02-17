016886a708c9b2aeb3c02dc157d6b6894bd1c6f2
cravo123 <cc3630@columbia.edu>
2019-02-17 18:15:13 -0500

    Create 0111. Minimum Depth of Binary Tree.py

---
+++ 0111. Minimum Depth of Binary Tree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# Recursion
+class Solution:
+    def dfs(self, node):
+        if node is None:
+            return 0
+        
+        if node.left is None:
+            return 1 + self.dfs(node.right)
+        if node.right is None:
+            return 1 + self.dfs(node.left)
+        
+        return 1 + min(self.dfs(node.left), self.dfs(node.right))
+        
+    def minDepth(self, root: 'TreeNode') -> 'int':
+        return self.dfs(root)
+
+# Iteration
+class Solution:
+    def minDepth(self, root: 'TreeNode') -> 'int':
+        level = 0
+        if root is None:
+            return level
+        
+        q = [root]
+        while q:
+            level += 1
+            for p in q:
+                if p.left is None and p.right is None:
+                    return level
+            q = [x for p in q for x in [p.left, p.right] if x]

8a4da316ea2e4956673b070dc0ad8e15e237c6d4
cravo123 <cc3630@columbia.edu>
2019-02-21 18:14:46 -0500

    Create 0500 Keyboard Row.py

---
+++ 0500 Keyboard Row.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findWords(self, words: 'List[str]') -> 'List[str]':
+        line1, line2, line3 = (set('qwertyuiop'), set('asdfghjkl'), set('zxcvbnm'))
+        res = []
+        for word in words:
+            words = word.lower()
+            if any(all(c in line for c in words) for line in [line1, line2, line3]):
+                res.append(word)
+        return res

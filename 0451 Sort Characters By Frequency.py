ad19c7c03334de91e42a45ede12d456498ba1f19
cravo123 <cc3630@columbia.edu>
2019-02-21 18:29:06 -0500

    Update 0451 Sort Characters By Frequency.py

--- 0451 Sort Characters By Frequency.py
+++ 0451 Sort Characters By Frequency.py
@@ -1,3 +1,4 @@
+# Solution 1, Sorting
 class Solution:
     def frequencySort(self, s: 'str') -> 'str':
         s = list(s)
@@ -8,3 +9,20 @@ class Solution:
         s = ''.join(s)
         
         return s
+# Solution 2, Bucket Sorting
+class Solution:
+    def frequencySort(self, s: 'str') -> 'str':
+        d = collections.Counter(s)
+        
+        bucket = [[] for _ in range(len(s) + 1)]
+        
+        for c, cnt in d.items():
+            bucket[cnt].append(c)
+        
+        res = []
+        
+        for i in range(len(bucket) - 1, -1, -1):
+            for c in bucket[i]:
+                res.append(c * i)
+        res = ''.join(res)
+        return res

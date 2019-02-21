4a1edb9af9ce2e6db885da79e70dd8ca6e00be7b
cravo123 <cc3630@columbia.edu>
2019-02-21 17:40:41 -0500

    Update 0942 DI String Match.py

--- 0942 DI String Match.py
+++ 0942 DI String Match.py
@@ -1,3 +1,4 @@
+# Solution 1,
 class Solution:
     def diStringMatch(self, S: 'str') -> 'List[int]':
         n = len(S)
@@ -14,4 +15,20 @@ class Solution:
                 j -= 1
             idx += 1
         res[idx] = i
+        return res
+
+# Solution 2,
+class Solution:
+    def diStringMatch(self, S: 'str') -> 'List[int]':
+        left = right = 0
+        res = [0]
+
+        for c in S:
+            if c == 'I':
+                right += 1
+                res.append(right)
+            else:
+                left -= 1
+                res.append(left)
+        res = [x - left for x in res]
         return res

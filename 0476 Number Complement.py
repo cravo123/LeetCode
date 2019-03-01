7eb4e9e9f2bc7335f3352cdbc56832eade0a3367
cravo123 <cc3630@columbia.edu>
2019-03-01 12:54:01 -0500

    Update 0476 Number Complement.py

--- 0476 Number Complement.py
+++ 0476 Number Complement.py
@@ -1,3 +1,4 @@
+# Solution 1
 class Solution:
     def findComplement(self, num: int) -> int:
         q = []
@@ -11,3 +12,11 @@ class Solution:
             res = res * 2 + (1 - v)
         
         return res
+
+# Solution 2, use mask
+class Solution(object):
+    def findComplement(self, num):
+        i = 1
+        while i <= num:
+            i = i << 1
+        return (i - 1) ^ num

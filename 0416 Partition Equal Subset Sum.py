affef9efc983b5c180053e1af7174d782c6b19d9
cravo <cc3630@columbia.edu>
2019-05-13 00:08:20 -0400

    2019.05.12

--- 0416 Partition Equal Subset Sum.py
+++ 0416 Partition Equal Subset Sum.py
@@ -19,6 +19,9 @@ class Solution:
         
         dp = [[False for _ in range(target + 1)] for _ in range(n + 1)]
         dp[0][0] = True
+
+        for i in range(1, n + 1):
+            dp[i][0] = True
         
         for i in range(1, n + 1):
             for j in range(1, target + 1):

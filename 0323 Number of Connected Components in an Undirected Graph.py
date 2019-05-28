4809e1730067c66f8c79c746c552112081798750
cravo <cc3630@columbia.edu>
2019-05-28 00:59:53 -0400

    2019.05.27

--- 0323 Number of Connected Components in an Undirected Graph.py
+++ 0323 Number of Connected Components in an Undirected Graph.py
@@ -2,28 +2,28 @@ import collections
 
 # Solution 1, DFS
 class Solution:
-    def dfs(self, idx, seen, d):
-        if idx in seen:
-            return
-        seen.add(idx)
-        for i in d[idx]:
-            self.dfs(i, seen, d)
+    def dfs(self, u, d, seen):
+        seen.add(u)
+        
+        for v in d[u]:
+            if v not in seen:
+                self.dfs(v, d, seen)
         
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         d = collections.defaultdict(set)
         
-        for a, b in edges:
-            d[a].add(b)
-            d[b].add(a)
+        for u, v in edges:
+            d[u].add(v)
+            d[v].add(u)
         
-        res = 0
+        cnt = 0
         seen = set()
+        for u in range(n):
+            if u not in seen:
+                cnt += 1
+                self.dfs(u, d, seen)
         
-        for i in range(n):
-            if i not in seen:
-                res += 1
-                self.dfs(i, seen, d)
-        return res
+        return cnt
 
 # Solution 2, BFS
 class Solution:
@@ -58,6 +58,56 @@ class Solution:
         return res
 
 # Solution 3, Union Find
+# We can abstract Union Find logic out as a separate class
+# This will make our implementation easier.
+class UFS:
+    def __init__(self):
+        self.idx = 0
+        self.d = {} # data -> idx
+        self.parent = {} # idx -> ulti-parent
+        self.size = 0
+        
+    def add_point(self, p):
+        if p not in self.d:
+            self.d[p] = self.idx
+            self.parent[self.idx] = self.idx
+            self.idx += 1
+            self.size += 1
+    
+    def find_parent(self, p):
+        i = self.d[p]
+        j = i
+        while j != self.parent[j]:
+            j = self.parent[j] # can be optimized by path-halving or path-compression
+
+            # path compression
+            #self.parent[j] = self.parent[self.parent[j]]
+            #j = self.parent[j]
+
+        self.parent[i] = j
+        
+        return j
+    
+    def union(self, p, q):
+        i, j = self.find_parent(p), self.find_parent(q)
+        if i != j:
+            self.parent[i] = j # can be optimized by using ranking, i.e. make tree as short as possible
+            self.size -= 1
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        ufs = UFS()
+        
+        for i in range(n):
+            ufs.add_point(i)
+        
+        for u, v in edges:
+            ufs.union(u, v)
+        
+        return ufs.size
+
+
+# Solution 3.1 Union Find
 class Solution:
     def parent(self, idx, d):
         if idx != d[idx]:

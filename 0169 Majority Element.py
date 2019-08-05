b14547b92383c54b8e3aca8e5e76fb0bd9008195
cravo <cc3630@columbia.edu>
2019-08-04 23:57:26 -0400

    2019.08.03

--- 0169 Majority Element.py
+++ 0169 Majority Element.py
@@ -1,19 +1,12 @@
-# Moore Voting Algorithm
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        v, cnt = nums[0], 0
+# Solution 1, Moore Voting Algorithm
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res, cnt = None, 0
         
         for c in nums:
-            if c == v:
-                cnt += 1
-            else:
-                cnt -= 1
+            if cnt == 0:
+                res = c
             
-            if cnt < 0:
-                v, cnt = c, 1
+            cnt += 1 if res == c else -1
         
-        return v
+        return res

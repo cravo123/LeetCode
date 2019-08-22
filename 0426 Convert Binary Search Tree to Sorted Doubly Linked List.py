afe17490ed411207aa39f8928bb83883b319530a
cravo <cc3630@columbia.edu>
2019-08-21 22:59:27 -0400

    2019.08.21

--- 0426 Convert Binary Search Tree to Sorted Doubly Linked List.py
+++ 0426 Convert Binary Search Tree to Sorted Doubly Linked List.py
@@ -15,52 +15,36 @@ class Solution:
         if node is None:
             return None, None
         
-        L, R = self.dfs(node.left), self.dfs(node.right)
+        l_h, l_t = self.dfs(node.left)
+        r_h, r_t = self.dfs(node.right)
         
-        if L[0] is None and R[0] is None:
+        if l_t:
+            head = l_h
+            l_t.right = node
+            node.left = l_t
+        else:
+            head = node
             node.left = None
+        
+        if r_h:
+            tail = r_t
+            node.right = r_h
+            r_h.left = node
+        else:
+            tail = node
             node.right = None
-            return node, node
-        
-        if L[0] is None:
-            node.left = None
-            node.right = R[0]
-            R[0].left = node
-            
-            return node, R[1]
-        if R[0] is None:
-            L[1].right = node
-            node.left = L[1]
-            
-            return L[0], node
-        
-        L[1].right = node
-        node.left = L[1]
-        node.right = R[0]
-        R[0].left = node
-        
-        return L[0], R[1]
-        
-    def treeToDoublyList(self, root: 'Node') -> 'Node':
-        head, tail = self.dfs(root)
-        
-        if head is None or tail is None:
-            return head
         
         head.left = tail
         tail.right = head
         
-        return head
+        return head, tail
+        
+    def treeToDoublyList(self, root: 'Node') -> 'Node':
+        res, _ = self.dfs(root)
+        
+        return res
 
 # Solution 2, in-order traversal
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val, left, right):
-        self.val = val
-        self.left = left
-        self.right = right
-"""
 class Solution:
     def treeToDoublyList(self, root: 'Node') -> 'Node':
         head = curr = None

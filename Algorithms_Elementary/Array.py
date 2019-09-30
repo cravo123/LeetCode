ebf6aba65b3b05099b863c65a064ab6fd2c2e022
cravo <cc3630@columbia.edu>
2019-09-29 23:05:47 -0400

    2019.09.29 Add tree traversal

--- Algorithms_Elementary/Array.py
+++ Algorithms_Elementary/Array.py
@@ -22,6 +22,7 @@ def bubble_sort(arr):
 # Maintain an invariant here, so for the k-th iteration, we make sure arr[:k] is sorted.
 # For each iteration, we find the place where the value should be inserted.
 # So after n iterations, the array will be sorted.
+# Time Complexity, O(n ^ 2)
 def insertion_sort(arr):
     arr = arr.copy()
     n = len(arr)
@@ -42,6 +43,7 @@ def insertion_sort(arr):
 # Here we sort the array of length n by first assuming we have sorted sub-array of length n / 2.
 # Remember in order for recursion to work, we need to have a base-case that can be solved without calling 
 # recursion function. For the array sorting problem, the base case is if array length is less than or equal to 1
+# Time Complexity, O(n log n)
 def merge_sort(arr):
     arr = arr.copy()
     n = len(arr)
@@ -73,7 +75,7 @@ def _merge(arr, start, mid, end):
 
 def _merge_sort_helper(arr, start, end):
     # Base case: if length <= 1
-    if start + 1 > end:
+    if start >= end:
         return
     
     mid = (start + end) // 2
@@ -88,7 +90,47 @@ def _merge_sort_helper(arr, start, end):
 # put every value >= pivot value to the right.
 # Variant of this algorithm can be applied to 
 #   1) Find the largest k-th value in O(n) time,
-#   2) Two-pointer, Three-pointer problem, like Dutch national flag problem
+#   2) Two-pointer, Three-pointer problem, like Dutch national flag problem，
+#      Notice that the find_pivot() function uses the idea of two-pointer.
+# Time Complexity, worst-case O(n ^2), average O(n Log n), 
+# But in most cases, quick sort is the fastest
+def quick_sort(arr):
+    arr = arr.copy()
+
+    quick_sort_helper(arr, 0, len(arr) - 1)
+
+    return arr
+
+def quick_sort_helper(arr, start, end):
+    # base case when length <= 1
+    if start >= end:
+        return
+
+    idx = find_pivot(arr, start, end)
+    quick_sort_helper(arr, start, idx - 1)
+    quick_sort_helper(arr, idx + 1, end)
+
+def find_pivot(arr, start, end):
+    mid = (start + end) // 2
+    v = arr[mid]
+    arr[mid], arr[end] = arr[end], arr[mid]
+
+    j = start
+    i = start
+    while i < end:
+        if arr[i] <= v:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
+        i += 1
+    arr[j], arr[end] = arr[end], arr[j]
+
+    return j
+
+# 1.5 Heap Sort
+# TODO:
+
+# 1.6 Counting Sort
+# TODO:
 
 
 
@@ -102,4 +144,5 @@ if __name__ == '__main__':
     print('Bubble Sort:    ', bubble_sort(arr))
     print('Insertion Sort: ', insertion_sort(arr))
     print('Merge Sort:     ', merge_sort(arr))
+    print('Quick Sort:     ', quick_sort(arr))
 
--- Algorithms_Elementary/Tree.py
+++ Algorithms_Elementary/Tree.py
@@ -8,7 +8,14 @@ class Node:
 # We use L, N, R to represent Left Child Node, Root Node, and Right Child Node. 
 # And visited order is shown by order in the resulted list.
 # There are 3 different traversal orders, all of which are distinguished by when Root Node is visited.
-# 1.1 pre-order Traversal, N -> L -> R
+# For recursion methods, it is trivial to see that the traversal order is determined by which order
+# each recursion function is called.
+# Speaking of relationship between tree traversal and BFS(Breadth-First-Search), DFS(Depth-First-Search)
+# Tree is a special case of graph, so 
+# Pre-oder, In-order and Post-order are DFS,
+# Level-traversal is BFS
+
+# 1.1 Pre-order Traversal, N -> L -> R
 def preorder_recursion_helper(node, res):
     if node is None:
         return
@@ -41,5 +48,52 @@ def preorder_iteration(root):
             q.append(p.left)
     
     return res
+
+# 1.2 In-order Traversal, L ->N -> R
+def inorder_recursion(root):
+    res = []
+    inorder_recursion_helper(root, res)
+
+    return res
+
+def inorder_recursion_helper(node, res):
+    if node is None:
+        return
+
+    inorder_recursion_helper(node.left, res)
+    res.append(node.val)
+    inorder_recursion_helper(node.right, res)
+
+# 1.3 Post-order Traversal, L ->R ->N
+def postorder_recursion(root):
+    res = []
+    postorder_recursion_helper(root, res)
+
+    return res
+
+def postorder_recursion_helper(node, res):
+    if node is None:
+        return
+
+    postorder_recursion_helper(node.left, res)
+    postorder_recursion_helper(node.right, res)
+    res.append(node.val)
+
+# 1.4 Level Traversal, traverse tree level by level
+# Like Breadth-First-Search
+def level_traversal_iteration(root):
+    res = []
+
+    if root is None:
+        return res
     
+    q = [root]
+
+    while q:
+        res.extend([p.val for p in q])
+        q = [x for p in q for x in [p.left, p.right] if x]
     
+    return res
+
+# Level traversal iteration
+# TODO:

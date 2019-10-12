cb6cf6ff6603bdd999705d1bbb943d6f52ef5010
cravo <cc3630@columbia.edu>
2019-10-11 23:48:52 -0400

    2019.10.11

--- Algorithms/0748 Shortest Completing Word.py
+++ Algorithms/0748 Shortest Completing Word.py
@@ -1,24 +1,6 @@
 import collections
 
-# Solution 1, with sorting
-class Solution:
-    def change(self, word):
-        d = collections.Counter()
-        for c in word:
-            if c.isalpha():
-                d[c.lower()] += 1
-        return d
-    
-    def shortestCompletingWord(self, licensePlate: str, words: List[str]) -> str:
-        target = self.change(licensePlate)
-        words.sort(key=lambda x:len(x))
-        for word in words:
-            d = self.change(word)
-        
-            if all(d[c] >= target[c] for c in target):
-                return word
-
-# Solution 2, without sorting
+# Solution s, without sorting
 class Solution:
     def change(self, word):
         d = collections.Counter()
